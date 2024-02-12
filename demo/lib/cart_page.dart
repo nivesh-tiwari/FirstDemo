de75f93db7ca65a82443b53815c3347b522ec7fb
unknown <niveshtiwari07@gmail.com>
2024-02-12 22:03:48 +0530

    First Commit

---
+++ demo/lib/cart_page.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+
+// class Product{
+
+// final int id;
+// final int price;
+// final String name;
+// final Color color;
+
+// Product({
+//     required this.id,
+//     required this.price,
+//     required this.name,
+//     required this.color,
+// });
+// }
+
+// final List<Product> PRODUCTS = [
+
+// Product(id: 0, price: 999, name: "leptop", color: Colors.red),
+// Product(id: 1, price: 10000, name: "tablet", color: Colors.black),
+// Product(id: 2, price: 700, name: "smart phone", color: Colors.lightGreenAccent ),
+// Product(id: 3, price: 500, name: "nokia", color: Colors.brown)
+
+
+
+
+// ];
---
+++ demo/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterProvider extends ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+
+  void increment(){
+    _counter++;
+    notifyListeners();
+  }
+}
+void main() {
+     runApp(
+      ChangeNotifierProvider(
+        create: (context)=> CounterProvider(),
+        child: MyApp(),
+  ),
+     );
+}
+// consume the provider in widget
+class MyWidget extends   StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CounterProvider>(
+      builder: (context, counterProvider, child){
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Counter: ${counterProvider.counter}'),
+              ElevatedButton(onPressed:(){
+                counterProvider.increment();
+              },
+              child: Text('Increment'),
+              )
+            ],
+          )
+          );
+      },
+    );
+  }
+}
+class MyApp extends  StatelessWidget {
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Provider Example'),
+        ),
+        body: MyWidget(),
+        ),
+      );
+  }
+  }
